36ba2098bf77400b92abc59b9fdc2505b8356e89
Shankymoti <shashankkumar648@gmail.com>
2026-01-16 23:52:48 +0530

    ts

---
+++ src/Typescript/Day2/comments.ts
@@ -0,0 +1,10 @@
+// Single line commnent. ---  Ctrl + /  for mac command+/
+/* 
+  Multi line comment. 
+  --- Shift + Alt + A for mac Shift + Option + A
+*/
+
+
+// console.log("Hello World!");
+
+/* console.log("multiple line comments") */
