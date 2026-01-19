04e474e0d48dfcb37b4e23e742f38787c6f57c2b
Shankymoti <shashankkumar648@gmail.com>
2026-01-19 19:43:14 +0530

    playwright

--- tests/Ex06_TextInputRadioButtonsCheckBoxes.spec.ts
+++ tests/Ex06_TextInputRadioButtonsCheckBoxes.spec.ts
@@ -1,6 +1,27 @@
-import {test, expect} from '@playwright/test';
+import {test, expect, Locator} from '@playwright/test';
 
 //Text Input/ Text Box/ Input Box
 // Radio Buttons
 //Check boxes
 
+test('Text Input Action',async ({page})=>{
+    await page.goto("https://testautomationpractice.blogspot.com/")
+
+    const textBox :Locator = page.locator('#name')
+
+    expect(textBox).toBeVisible()
+    expect(textBox).toBeEnabled()
+
+    let length : string | null = await textBox.getAttribute('maxlength')//// Returns value of maxlength attribute of the element
+    expect(length).toBe('15')
+
+    await textBox.fill("Shashank")
+
+    //console.log("text content of FirstName :", await textBox.textContent()); // returns empty
+
+    const firstName :  string =  await textBox.inputValue();
+    console.log("first name is ",firstName);
+    expect(firstName).toBe('Shashank')
+
+    await page.waitForTimeout(30000)
+})
