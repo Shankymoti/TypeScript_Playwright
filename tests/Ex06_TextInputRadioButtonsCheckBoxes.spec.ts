e999498b2513059d61bae7630a0bfcd96c133012
Shankymoti <shashankkumar648@gmail.com>
2026-01-20 22:58:06 +0530

    TextInputRadioButton

--- tests/Ex06_TextInputRadioButtonsCheckBoxes.spec.ts
+++ tests/Ex06_TextInputRadioButtonsCheckBoxes.spec.ts
@@ -25,3 +25,83 @@ test('Text Input Action',async ({page})=>{
 
     await page.waitForTimeout(30000)
 })
+
+// Radio Button
+ test('Radio Button Action', async({page})=>{
+    await page.goto('https://testautomationpractice.blogspot.com/')
+   const maleRadio :Locator =  page.locator('#male')
+
+   await expect(maleRadio).toBeVisible()
+   await expect(maleRadio).toBeEnabled()
+   expect(await maleRadio.isChecked()).toBe(false)
+
+   await maleRadio.check()
+   expect(await maleRadio.isChecked()).toBe(true)
+   await expect(maleRadio).toBeChecked()
+})
+
+// CheckBox
+test('Checkbox Action', async ({page})=>{
+     await page.goto('https://testautomationpractice.blogspot.com/')
+
+      // 1. Select specific checkbox (Sunday) using getByLabel and assert
+
+    //   const sundayCheckbox : Locator =  page.getByLabel('Sunday')
+    //   await sundayCheckbox.check()
+    //   await expect(sundayCheckbox).toBeChecked()
+    //   await page.waitForTimeout(3000)
+
+      // 2. Select all checkboxes and assert each is checked
+      const days : string[] = ['Sunday','Monday', 'Tuesday','Wednesday', 'Thursday', 'Friday','Saturday']
+
+      const checkboxes : Locator[] = days.map((index)=> page.getByLabel(index))
+      expect(checkboxes.length).toBe(7)
+
+    //   for(let checkbox of checkboxes){
+    //     await checkbox.check()
+    //     await expect(checkbox).toBeChecked()
+    //   }
+
+
+     // 4. Uncheck last 3 checkboxes and assert
+
+    //  for(let checkbox of checkboxes.slice(-3)){
+    //     await checkbox.uncheck()
+    //     await expect(checkbox).not.toBeChecked()
+    //  }
+     
+
+     ////5.    Toggle checkboxes: If checked, uncheck; if unchecked, check. Assert state flipped.
+
+    //  for(let checkbox of checkboxes){
+    //     if(await checkbox.isChecked()){
+    //         await checkbox.uncheck()
+    //         await expect(checkbox).not.toBeChecked()
+    //     }else{
+    //         await checkbox.check()
+    //         await expect(checkbox).toBeChecked()
+    //     }
+    //  }
+     
+     // 6. Randomely select check boxes - Select checkboxes by index (1, 3, 6) and assert
+
+    //  const index : number[] = [1,3,5]
+    //  for(let i of index){
+    //     await checkboxes[i].check()
+    //     await expect(checkboxes[i]).toBeChecked()
+    //  }
+
+     //await page.waitForTimeout(30000)
+
+     ////7. Select the check box based on the Label
+
+     const weekname : string = "Friday"
+
+     for( let label of days){
+        if(label.toLocaleLowerCase() == weekname.toLocaleLowerCase()){
+          const day : Locator =  page.getByLabel(label)
+          await day.check()
+          await expect(day).toBeChecked()
+        }
+     }
+})
