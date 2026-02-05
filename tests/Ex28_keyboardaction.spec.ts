c935d2c5c28e361d361d0db2f1313d68aab0112c
Shankymoti <shashankkumar648@gmail.com>
2026-02-05 23:01:14 +0530

    keyboard action

---
+++ tests/Ex28_keyboardaction.spec.ts
@@ -0,0 +1,173 @@
+import {test,expect,Locator} from '@playwright/test'
+
+test('keyboard action', async({page})=>{
+    await page.goto("https://testautomationpractice.blogspot.com/")
+    const input1 : Locator = page.locator('#input1')
+    await input1.focus()
+
+    await page.keyboard.insertText('welcome')
+
+ 
+
+
+//3) Ctrl + A - select the text from input1
+await page.keyboard.down('Meta');
+await page.keyboard.press('A');
+await page.keyboard.up('Mata');
+
+//4) Ctrl +C - copy the text from input 1.
+await page.keyboard.down('Meta');
+await page.keyboard.press('C');
+await page.keyboard.up('Meta');
+
+
+//5 Press TAB - 2 times
+await page.keyboard.press('Tab');
+await page.keyboard.press('Tab');
+
+
+//6) Ctrl + V  - past the text in input2
+await page.keyboard.down('Meta');
+await page.keyboard.press('V');
+await page.keyboard.up('Meta');
+
+
+//7) Press TAB - 2 times
+await page.keyboard.press('Tab');
+await page.keyboard.press('Tab');
+
+
+//8) Ctrl+V - past the text in input 3
+await page.keyboard.down('Meta');
+await page.keyboard.press('V');
+await page.keyboard.up('Meta');
+
+await page.waitForTimeout(5000);
+
+})
+
+test('Keyboard action - simple way',async({page})=>{
+    await page.goto("https://testautomationpractice.blogspot.com/")
+   const input1 : Locator = page.locator('#input1')
+    await input1.focus()
+
+    await page.keyboard.insertText('welcome')
+
+    await page.keyboard.press('Meta+A')
+    await page.keyboard.press('Meta+C')
+
+    await page.keyboard.press('Tab')
+
+    await page.keyboard.press('Meta+V')
+
+    await page.keyboard.press('Tab')
+  await page.keyboard.press('Tab')
+
+
+    await page.keyboard.press('Meta+V')
+
+    await page.waitForTimeout(5000)
+})
+
+/*
+
+// Keyboard methods:
+// insertText
+// down
+// press
+// type
+// up
+
+// await page.keyboard
+
+import { test, expect } from '@playwright/test';
+
+test('keyboard actions', async({page})=>{
+
+await page.goto("https://testautomationpractice.blogspot.com/");
+
+const input1=page.locator('#input1');
+
+//1) fous on input1
+await input1.focus(); //await input1.click();
+
+//2) provide the text input1
+await page.keyboard.insertText("welcome");
+
+//3) Ctrl + A - select the text from input1
+await page.keyboard.down('Control');
+await page.keyboard.press('A');
+await page.keyboard.up('Control');
+
+//4) Ctrl +C - copy the text from input 1
+await page.keyboard.down('Control');
+await page.keyboard.press('C');
+await page.keyboard.up('Control');
+
+
+//5 Press TAB - 2 times
+await page.keyboard.press('Tab');
+await page.keyboard.press('Tab');
+
+
+//6) Ctrl + V  - past the text in input2
+await page.keyboard.down('Control');
+await page.keyboard.press('V');
+await page.keyboard.up('Control');
+
+
+//7) Press TAB - 2 times
+await page.keyboard.press('Tab');
+await page.keyboard.press('Tab');
+
+
+//8) Ctrl+V - past the text in input 3
+await page.keyboard.down('Control');
+await page.keyboard.press('V');
+await page.keyboard.up('Control');
+
+await page.waitForTimeout(5000);
+
+})
+
+
+test.only('keyboard actions - simple way', async({page})=>{
+
+await page.goto("https://testautomationpractice.blogspot.com/");
+
+const input1=page.locator('#input1');
+
+//1) fous on input1
+await input1.focus(); //await input1.click();
+
+//2) provide the text input1
+await page.keyboard.insertText("welcome");
+
+//3) Ctrl + A - select the text from input1
+await page.keyboard.press('Control+A');
+
+//4) Ctrl +C - copy the text from input 1
+await page.keyboard.press('Control+C');
+
+//5 Press TAB - 2 times
+await page.keyboard.press('Tab');
+await page.keyboard.press('Tab');
+
+
+//6) Ctrl + V  - past the text in input2
+await page.keyboard.press('Control+V');
+
+
+//7) Press TAB - 2 times
+await page.keyboard.press('Tab');
+await page.keyboard.press('Tab');
+
+
+//8) Ctrl+V - past the text in input 3
+await page.keyboard.press('Control+V');
+
+await page.waitForTimeout(5000);
+
+})
+
+*/
